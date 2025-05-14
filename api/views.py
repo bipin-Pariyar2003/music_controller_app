0e4e575c0313d8dba210a1f680cbae0db5f77121
bipin-Pariyar2003 <163013814+bipin-Pariyar2003@users.noreply.github.com>
2025-05-14 12:10:43 +0545

    initialized models and databases

--- api/views.py
+++ api/views.py
@@ -4,5 +4,5 @@ from django.http import HttpResponse
 # Create your views here.
 def main(request): 
     # return render(request, 'index.html')
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("<h1>Hello, world. </h1>")
  
